4d057bae7137910a22873415ae6f7b4179f944ad
Yogesh  Sambare <bloggingscan@gmail.com>
2022-01-25 21:54:44 +0530

    updated

--- linkedin_clone/components/HeaderLink.js
+++ linkedin_clone/components/HeaderLink.js
@@ -1,7 +1,12 @@
-function HeaderLink(props) {
+function HeaderLink({ Icon, text, avatar }) {
   return ( 
-  <div>
-{props.text}
+  <div className="cursor-pointer flex flex-col justify-center items-center">
+  {avatar ? (
+<Icon className="!h=7 !w=7 lg:!mb-1" />
+  ): (
+      <Icon/>
+  )}
+  <h4 className="text-sm">{text}</h4>
   </div>
   )
 }
