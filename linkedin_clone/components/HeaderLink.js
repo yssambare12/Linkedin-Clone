1c0c31d5a5c3d6f6e46f5d838e338dc91d34f1d5
Yogesh  Sambare <bloggingscan@gmail.com>
2022-01-29 21:45:54 +0530

    Update HeaderLink.js

--- linkedin_clone/components/HeaderLink.js
+++ linkedin_clone/components/HeaderLink.js
@@ -1,6 +1,6 @@
 function HeaderLink({ Icon, text, avatar, feed }) {
   return ( 
-  <div className={`cursor-pointer flex flex-col justify-center items-center `}>
+  <div className={`cursor-pointer flex flex-col justify-center items-center ${feed ? "text-black/60 hover:text-black dark:text:white 75dark:hover:text-white lg:-mb-1.5 space-y-1" : "text-gray-500 hover:text-gray-700"}`}>
   {avatar ? (
 <Icon className="!h=7 !w=7 lg:!mb-1" />
   ): (
