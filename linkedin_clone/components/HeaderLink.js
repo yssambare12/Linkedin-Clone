c748407a999093f129898aff7d505fc706bac97c
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2022-02-13 22:17:04 +0530

    Update HeaderLink.js

--- linkedin_clone/components/HeaderLink.js
+++ linkedin_clone/components/HeaderLink.js
@@ -5,7 +5,7 @@ function HeaderLink({ Icon, text, avatar, feed }) {
   "text-black/60 hover:text-black dark:text:white 75dark:hover:text-white lg:-mb-1.5 space-y-1" :
    "text-gray-500 hover:text-gray-700"}`}>
   {avatar ? (
-<Icon className="!h=7 !w=7 lg:!mb-1" />
+<Icon className="!h=7 !w=7 lg:!mb-3" />
   ): (
       <Icon/>
   )}
